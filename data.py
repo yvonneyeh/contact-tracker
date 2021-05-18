f11acfd682350a87b0cb37fbec72ec5cef5b55d6
Yvonne Yeh <code@yvonneyeh.com>
2021-05-18 13:33:13 -0700

    Add sample data

--- data.py
+++ data.py
@@ -6,3 +6,61 @@ users = {'Alex': ['Becky', 'Erica', 'Chris', 'Grace'],
         'Erica': ['Alex', 'Becky', 'Danny'],
         'Frank': ['Chris', 'Becky'],
         'Grace': ['Alex', 'Becky', 'Erica', 'Chris']}
+
+details  = {
+    {'email': 'alex@test.com',
+    'username': 'aa',
+    'first_name': 'Alex',
+    'last_name': 'A.',
+    'city': 'Albany',
+    'state': 'NY'
+    },
+    {
+    {'email': 'becky@test.com',
+    'username': 'bb',
+    'first_name': 'Becky',
+    'last_name': 'B.',
+    'city': 'Brooklyn',
+    'state': 'NY'
+    },
+    {
+    {'email': 'chris@test.com',
+    'username': 'cc',
+    'first_name': 'Chris',
+    'last_name': 'C.',
+    'city': 'Cleveland',
+    'state': 'OH'
+    },
+    {
+    {'email': 'danny@test.com',
+    'username': 'dd',
+    'first_name': 'Danny',
+    'last_name': 'D.',
+    'city': 'Dallas',
+    'state': 'TX'
+    },
+    {
+    {'email': 'erica@test.com',
+    'username': 'ee',
+    'first_name': 'Erica',
+    'last_name': 'E.',
+    'city': 'Eugene',
+    'state': 'OR'
+    },
+    {
+    {'email': 'frank@test.com',
+    'username': 'ff',
+    'first_name': 'Frank',
+    'last_name': 'F.',
+    'city': 'Frankfurt',
+    'state': 'DEU'
+    },
+    {
+    {'email': 'grace@test.com',
+    'username': 'gg',
+    'first_name': 'Grace',
+    'last_name': 'G.',
+    'city': 'Giza',
+    'state': 'EGY'
+    },
+}
