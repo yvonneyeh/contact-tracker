17df71bab172930a23eb9686aad27d645121540e
Yvonne Yeh <code@yvonneyeh.com>
2021-05-17 18:30:30 -0700

    Create Breadth-First Search function to find shortest path between 2 users

---
+++ bfs.py
@@ -0,0 +1,47 @@
+from data import users
+
+def BFS(graph, user1, user2):
+    """ Find shortest path between 2 user nodes in a graph"""
+    explored = []
+     
+    # Queue for traversing the graph in the BFS
+    queue = [[user1]]
+     
+    # If the desired user is already found
+    if user1 == user2:
+        print("Same User")
+        return 0
+     
+    # Loop to traverse the graph with the help of the queue
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+         
+        # Condition to check if the current node is not visited
+        if node not in explored:
+            neighbors = graph[node]
+             
+            # Loop to iterate over the neighbors of the node to find mutual connections
+            for neighbor in neighbors:
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+                # print(new_path)
+                # print(len(new_path))
+                 
+                # Condition to check if the neighbor node is the target
+                if neighbor == user2:
+                    print("Shortest path:", *new_path) 
+                    return len(new_path) # Degrees of separation
+            explored.append(node)
+ 
+    # Condition when the nodes are not connected
+    print("A connection between these users does not exist.")
+    return -1
+ 
+
+# Driver Code
+if __name__ == "__main__":
+
+    # Function Call
+    BFS(users, 'Alex', 'Danny')
---
+++ data.py
@@ -0,0 +1,8 @@
+# Graph using dictionaries
+users = {'Alex': ['Becky', 'Erica', 'Chris', 'Grace'],
+        'Becky': ['Alex', 'Chris', 'Erica'],
+        'Chris': ['Alex', 'Frank', 'Grace'],
+        'Danny': ['Becky', 'Erica'],
+        'Erica': ['Alex', 'Becky', 'Danny'],
+        'Frank': ['Chris', 'Becky'],
+        'Grace': ['Alex', 'Becky', 'Erica', 'Chris']}
